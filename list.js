d2fc9d40eb7d034b908a3f39ce2ecc71456b4681
Mustafa Dokumaci <mustafa@foraa.com>
2016-10-24 11:30:06 +0200

    export list of devices & platforms

---
+++ list.js
@@ -0,0 +1,5 @@
+'use strict'
+
+exports.devices = ['desktop', 'tablet', 'phone', 'console', 'tv', 'cast', 'firetv', 'sticktv', 'rikstv']
+
+exports.platforms = ['windows', 'linux', 'lg', 'firefox', 'mac', 'ios', 'xbox', 'playstation', 'android', 'cast', 'samsung', 'rikstv']
